9560e9fe10cb4b52bdfc0040373ce12fc7f981b0
Harin Kaklotar <harinlive@gmail.com>
2017-08-05 17:15:12 +0530

    add some math and number examples

---
+++ ex3.py
@@ -0,0 +1,31 @@
+# Number and math
+
+print "now I will count my tex"
+
+print "tex", 25 + 30 / 6   	# for calculate tex
+print "service tex", 100 - 25 * 3 % 4  # for calculate service tex
+
+print "now we performe some basic math oprtaion"
+
+print "addition 5 + 7 = ", 5 + 7	# addition
+
+print "subtraction 7 - 3 = ", 7 - 3  # subtration
+
+print "multiplication 9 * 3 = ", 9 * 3 # multiplication
+
+print "division 11 / 2 = ", 11 / 2  # example of division
+
+print "modulo 11 % 2 = ", 11 % 2 # modulo opration
+
+# well now we perform complex example
+
+print "4+5-3/5+6*4+11",4+5-3/5+6*4+11 
+
+print "lets check some other things using maths"
+
+print "Is 5 is greter than 2 ? ", 5 > 2
+
+print " is 5 == 4", 5 == 4
+
+print "is 5 != 4", 5 != 4
+
