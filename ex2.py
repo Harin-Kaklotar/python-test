466d4ad50e3185e61af82bc323ffe7b2188e6ee3
Harin Kaklotar <harinlive@gmail.com>
2017-08-05 16:47:45 +0530

    add some comment demo

--- ex2.py
+++ ex2.py
@@ -1,1 +1,11 @@
 # comment line demo
+# Anything after # is ignore by pythone
+
+print "Hello harin this is python comment demo" # this is simple print function
+
+bus = 10  # this is bus variable
+
+# you can also use a comment to 'disable' or comment out a puece of code:
+# print "This won't run"
+
+print "This will run"
