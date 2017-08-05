216b2e7b195f8a11af78504c458e49397a6ed51a
Harin Kaklotar <harinlive@gmail.com>
2017-08-05 17:34:41 +0530

    some example of python variable

---
+++ ex4_variable_and_name.py
@@ -0,0 +1,27 @@
+# Variable and Names
+
+print "here we do some practice on python variable and name"
+
+cars = 45
+space_in_car = 4.0
+driver = 30
+passangers = 90
+car_not_driven = cars - driver
+car_driven = driver
+car_pool_capicity = car_driven * space_in_car
+average_passagners_per_car = passangers / car_driven
+
+print "there are", cars, "cars available"
+print "there are", driver, "drivers available"
+print "so there will be", car_not_driven, "empty cars today"
+print "we can transpot", car_pool_capicity,"people tooday"
+print "we have",passangers,"passagner to carpool today"
+print "we need to put about", average_passagners_per_car,"in each car."
+
+# ok now do some other stuff
+x = 10   # variable x
+y = 15   # variable y
+
+print "x = ", x
+print "y = ", y
+print "x + y=", x+y
