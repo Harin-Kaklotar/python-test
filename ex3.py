60171a8d495840873e54518c38428317e5abf183
Harin Kaklotar <harinlive@gmail.com>
2017-08-05 17:18:33 +0530

    add some floatin math operation

--- ex3.py
+++ ex3.py
@@ -12,8 +12,10 @@ print "addition 5 + 7 = ", 5 + 7	# addition
 print "subtraction 7 - 3 = ", 7 - 3  # subtration
 
 print "multiplication 9 * 3 = ", 9 * 3 # multiplication
+print "again 915.014*235.012", 915.014*235.012 
 
 print "division 11 / 2 = ", 11 / 2  # example of division
+print "again 5.0/2.0 = ", 5.0/2.0
 
 print "modulo 11 % 2 = ", 11 % 2 # modulo opration
 
@@ -28,4 +30,6 @@ print "Is 5 is greter than 2 ? ", 5 > 2
 print " is 5 == 4", 5 == 4
 
 print "is 5 != 4", 5 != 4
+
+
 
