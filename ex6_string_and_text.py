3dc9854117de1b7d8364447d34429313d5d87d1c
Harin Kaklotar <harinlive@gmail.com>
2017-08-05 20:24:09 +0530

    add extra string and text examples

--- ex6_string_and_text.py
+++ ex6_string_and_text.py
@@ -1,3 +1,34 @@
 # String and Text
 
-print "Hello ex6.py"
+'''
+This is a multiline
+comment.
+'''
+"Hi this is also one line comment"
+
+"""
+another multi line comment
+hello harin 
+how are you!!!!
+"""
+
+x = "There are %d types of people." % 10
+binary = 'binary'
+do_not = "don't"
+y = "Those who know %s and those who %s" %(binary, do_not)
+
+print x
+print y
+
+print "I said %s" %x
+print "And also said %s" %y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny ? %r"
+
+print joke_evaluation %hilarious
+
+w = "This is a left size of ..."
+e = "a string with a righ side."
+
+print w + e
