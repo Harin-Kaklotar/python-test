0bf222823557eb2f541552493f122d9c1e213e26
Harin Kaklotar <harinlive@gmail.com>
2017-08-10 12:45:47 +0530

    another printing exercise

---
+++ ex9_print_print_print.py
@@ -0,0 +1,45 @@
+# print print print
+
+# In gujarat,India day name be like this :D
+days = "Somvar Mangalvar Budhvar Gurvar Shukravar Shanivar Ravivar"
+'''
+don't worry its like 
+Somvar = Monday 
+Mangalvar = Tuesday 
+Budhvar = Wednusday 
+Gurvar = Thursday 
+Shukravar = Friday 
+Shanivar = Saturday 
+Ravivar = Sunday
+
+note: If you want to learn gujarati language please contact me ;)
+      Actually I want to lear also french, Spanish, German, Italy, 
+'''
+# ok back to the points
+months = "Jan\nFab\nMarch\nApril\nMay\nJun\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember" 
+
+print "Here are the days:", days
+#print "Here are the days: %s" %days   # don't worry both are valid. Just uncomment this and check it out
+print "Here are the months: ", months
+
+'''
+Her you see that in output every months name are in new line , hmmmmmmm
+do you knnow why???
+just check the month line where we add all the months 
+have you check it???
+have you found something new??? yesssss???
+hmmm it's  '\n' between the month name
+yuup '\n' represent new line
+when ever you write \n in string interpritor give output in new line
+
+note: '\n' is literal in python there are also some other pthon like below
+>>>>  \n \t \b \a \f
+'''
+
+# nooooooooow we print paragraph 
+print '''
+ Hello everyone!!!!
+ Tell me about your self
+ I like to make a new friends
+ Do you know python use interpreter not compiler??
+'''
