293cb97f4772147dbbe6bffafbe3d5b454722ee9
Harin Kaklotar <harinlive@gmail.com>
2017-08-10 11:24:19 +0530

    more printnig excercise

---
+++ ex7_more_printing.py
@@ -0,0 +1,46 @@
+# more printing
+
+print "I want to go landon once"
+print "Everyone said that it's so %s." %'beautiful' 
+print 'There are some many beautiful place' # here i use single quote
+
+# above examples are string type examples
+
+# now we write some character examples
+
+# we can use single or double (opening and closing) quote 
+# 
+
+# this all variable contain specific char value
+
+char_value1 = "H"
+char_value2 = "a"
+char_value3 = 'r'		# see here we can use signle quote also
+char_value4 = "i"
+char_value5 = "n"
+
+char_value6 = "K"
+char_value7 = "a"
+char_value8 = "k"
+char_value9 = "l"
+char_value10 = "o"
+char_value11 = "t"
+char_value12 = "a"
+char_value13 = "r"
+
+
+# now print all the characters
+print char_value1 + char_value2 + char_value3 + char_value4 +char_value5 + char_value6 + char_value7 + char_value8 + char_value9 + char_value10 + char_value11 + char_value12 + char_value13
+# so we get result like this > HarinKaklotar
+
+# but for code redability we can prin like this also
+print char_value1 + char_value2 + char_value3 + char_value4 +char_value5,
+print char_value6 + char_value7 + char_value8 + char_value9 + char_value10 + char_value11 + char_value12 + char_value13
+
+
+# but if you should try like this it will give type error
+'''
+print char_value1 + char_value2 + char_value3 + char_value4 +char_value5,
+print char_value6 + char_value7 + char_value8 + char_value9 + char_value10
+      + char_value11 + char_value12 + char_value13
+'''
