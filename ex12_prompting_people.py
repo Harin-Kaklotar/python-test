e6a525b86a81b45407d448f7ddd7a77705a7ade0
HarinKaklotar <harinlive@gmail.com>
2017-10-12 00:15:39 +0530

    new raw_input() exercise

---
+++ ex12_prompting_people.py
@@ -0,0 +1,16 @@
+# prompting people
+# actually we complete this part in ex11.py
+
+print "ok now start the exercise"
+
+y = raw_input("name ?")
+
+print y
+
+print "lets play a game"
+
+print "are you ready?"
+
+isReady = raw_input()
+
+print isReady
